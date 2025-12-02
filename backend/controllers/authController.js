23de7318d375fab678e052c16bce5e86cf3be84b
ullash-rahman <ullashrahman21@gmail.com>
2025-12-02 16:59:08 +0600

    w

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+
 // Register new user
 const register = async (req, res) => {
     try {
